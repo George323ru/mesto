a657c8a4043f798ab5398ef131141c33d6511e14
Georgiy Gusev <gus3225@yandex.ru>
2021-01-25 18:24:09 +0500

    slyle: написать код изменения текста в блоке description

--- scripts/main.js
+++ scripts/main.js
@@ -1,11 +1,38 @@
 const popup = document.querySelector('.popup');
 const popupOpenButton = document.querySelector('.profile__edit-button');
-const popupCloseButton = popup.querySelector('.popup__close-buttom');
+const popupCloseButton = popup.querySelector('.popup__close-button');
+const popupSaveButton = popup.querySelector('.popup__save-button');
 
-const popupToggle = function() {
+let userName = document.querySelector('.profile__user-name');
+let userSpec = document.querySelector('.profile__user-specialization');
+let inputName = popup.querySelector('.popup__input-name');
+let inputSpec = popup.querySelector('.popup__input-specialization');
+
+const popupToggle = function (evt) {
+  evt.preventDefault();
   popup.classList.toggle('popup_open');
+  inputName.value = userName.textContent;
+  inputSpec.value = userSpec.textContent;
+
+
 }
+
+
+const popupSave = function () {
+  // evt.preventDefault();
+  popup.classList.toggle('popup_open');
+
+  userName.innerHTML = ` ${inputName.value} `;
+  userSpec.innerHTML = ` ${inputSpec.value} `;
+
+}
+// userName.insertAdjacentText('afterbegin', `
+// ${inputName.value}
+// `);
+
 
 popupOpenButton.addEventListener('click', popupToggle);
 popupCloseButton.addEventListener('click', popupToggle);
+popupSaveButton.addEventListener('click', popupSave);
 
+// console.log(inputName)
