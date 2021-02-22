4e3fb67121e410ca21219fae291fd821f036b164
Georgiy Gusev <gus3225@yandex.ru>
2021-02-22 19:49:38 +0500

    feat: сделать подчеркивание инпута при ошибке

--- scripts/validate.js
+++ scripts/validate.js
@@ -3,20 +3,36 @@
 // Показываем сообщение с ошибкой
 const showInputError = (formElement, inputElement, errorMessage) => {
   // console.log(errorMessage)
-  // Находим элемент span для текста ошибки
+  // Находим элемент span с id как у инпута для текста ошибки
   const errorElement = formElement.querySelector(`#${inputElement.id}-error`);
-  inputElement.classList.add('form__input_type_error');
+  inputElement.classList.add('popup__input_type_error');
   errorElement.textContent = errorMessage;
-  errorElement.classList.add('form__input-error_active');
+  // Добавляем класс для появления ошибки
+  errorElement.classList.add('popup__input-error_active');
 };
 
 // Прячем сообщение с ошибкой
 const hideInputError = (formElement, inputElement) => {
   const errorElement = formElement.querySelector(`#${inputElement.id}-error`);
-  inputElement.classList.remove('form__input_type_error');
-  errorElement.classList.remove('form__input-error_active');
+  inputElement.classList.remove('popup__input_type_error');
+  errorElement.classList.remove('popup__input-error_active');
   errorElement.textContent = '';
 };
+
+// Обработчик состояния кнопки
+const toggleButtonState = (inputList, buttonElement) => {
+  if (hasInvalidInput(inputList)) {
+    buttonElement.classList.add('popup__submit_inactive');
+  } else {
+    buttonElement.classList.remove('popup__submit_inactive');
+  }
+}
+
+const hasInvalidInput = (inputList) => {
+  return inputList.some((inputElement) => {
+    return !inputElement.validity.valid;
+  })
+}
 
 // Проверяем "инпуты" на валидность
 const checkInputValidity = (formElement, inputElement) => {
@@ -28,20 +44,6 @@ const checkInputValidity = (formElement, inputElement) => {
   }
 };
 
-// const toggleButtonState = (inputList, buttonElement) => {
-//   if (hasInvalidInput(inputList)) {
-//     buttonElement.classList.add('form__submit_inactive');
-//   } else {
-//     buttonElement.classList.remove('form__submit_inactive');
-//   }
-// }
-
-// const hasInvalidInput = (inputList) => {
-//   return inputList.some((inputElement) => {
-//     return !inputElement.validity.valid;
-//   })
-// }
-
 const setEventListeners = (formElement) => {
   // Отменяем стандартную отправку формы
   formElement.addEventListener('submit', (evt) => {
@@ -50,9 +52,9 @@ const setEventListeners = (formElement) => {
 
   // Создаем массив из всех "инпутов"
   const inputList = Array.from(formElement.querySelectorAll('.popup__input'));
-  // const buttonElement = formElement.querySelector('.popup__save-button')
+  const buttonElement = formElement.querySelector('.popup__saveBtn')
 
-  // toggleButtonState(inputList, buttonElement);
+  toggleButtonState(inputList, buttonElement);
 
   // На каждый "инпут" навешиваем событие
   inputList.forEach(inputElement => {
